e87b7a407960709718314df2fde39f8329c5ed3c
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-16 11:12:48 -0600

    models and stuffs

--- keepr-client/src/AuthConfig.js
+++ keepr-client/src/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-v48qe94c.us.auth0.com'
+export const clientId = 'YjQDK2KDt6cWli7MXMO7smjfZXLgmu17'
+export const audience = 'KeepColey.com'
