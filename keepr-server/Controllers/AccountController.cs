2fe53232f5747692a157ff7d7a5533008346d64b
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-16 10:37:26 -0600

    init

---
+++ keepr-server/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using keeprcoley.Models;
+using keeprcoley.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace keepr_server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    // [Authorize]
+    public class AccountController : ControllerBase
+    {
+        private readonly ProfilesService _ps;
+        // private readonly ReviewsService _reviewserv;
+
+        public AccountController(ProfilesService ps)
+        {
+            _ps = ps;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Profile>> Get()
+        {
+            try
+            {
+                Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+                return Ok(_ps.GetOrCreateProfile(userInfo));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        // [HttpGet("reviews")]
+        // public async Task<ActionResult<IEnumerable<Review>>> GetReviewsByAccountId()
+        // {
+        //     try
+        //     {
+        //         Profile userInfo = await HttpContext.GetUserInfoAsync<Profile>();
+        //         return Ok(_reviewserv.GetReviewsByAccountId(userInfo.Id));
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         return BadRequest(e.Message);
+        //     }
+        // }
+    }
+}
