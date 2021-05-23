bc1970f9eb881e11f55dc731d10de4b3fd7f5819
ColeyDohmen <coley.dawn12@gmail.com>
2021-05-23 14:20:24 -0600

    text update

--- keepr-client/src/components/AddVaultModal.vue
+++ keepr-client/src/components/AddVaultModal.vue
@@ -12,7 +12,7 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">
-              Add Vault
+              Add to Vault
             </h5>
             <button
               type="button"
--- keepr-client/src/pages/AboutPage.vue
+++ keepr-client/src/pages/AboutPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is the about page</h1>
+    <h1></h1>
     <img class="rounded" :src="frog-eyes-chubby-frog-flower-full-width.jpg" alt="" />
   </div>
 </template>
